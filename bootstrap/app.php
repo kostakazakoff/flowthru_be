7a2bc91b61b3cf795f008e00698568bc69057371
Kosta Kazakov <kostakazakoff@gmail.com>
2024-03-24 00:40:05 +0200

    instaled Telescope & SANCTUM_STATEFUL_DOMAINS in .env change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
